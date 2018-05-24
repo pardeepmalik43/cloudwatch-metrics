05a296a598d79353209317789b942d638cd86bc1
root <pardeepmalik43@gmail.com>
2018-05-24 15:11:45 +0530

    Modifications in Script

--- cloud-metrics.sh
+++ cloud-metrics.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
+"#################**********************************************************######################################"
+echo "#                                                                                                               #"
+echo "# 		Welcome!! This script will  install Cloud Watch Metrics                                       #"
+echo "#                                                       						      #"
+echo "#################**********************************************************######################################"
 
 #Update ubuntu
 apt-get update
 
+echo -e "\n\n\n"
+echo " 				Updating System files... "
+echo -e "\n\n\n"
+
+
 ##Install zip unzip & perl 
-apt-get install libwww-perl libdatetime-perl zip unzip -y
+apt-get install libwww-perl libdatetime-perl curl zip unzip -y
 
 #Monitor Script
+cd ~/
 curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
 
 #Unzip CloudWatch Script
 unzip CloudWatchMonitoringScripts-1.2.2.zip 
+
+cd ~/aws-scripts-mon
+
+echo -e "\n\n\n"
 
 #Check reported metrics to CloudWatch
 ./mon-put-instance-data.pl --mem-used-incl-cache-buff --mem-util --mem-used --mem-avail
 
 
 ##Set Cronjob in Crontab
-(crontab -l ; echo "*/5 * * * * ~/aws-scripts-mon/mon-put-instance-data.pl --mem-used-incl-cache-buff --mem-util --disk-space-util --disk-path=/ --from-cron") | crontab -
+(echo "*/5 * * * * ~/aws-scripts-mon/mon-put-instance-data.pl --mem-used-incl-cache-buff --mem-util --disk-space-util --disk-path=/ --from-cron") | crontab -
+
 
